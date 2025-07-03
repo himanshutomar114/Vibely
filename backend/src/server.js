df6f86c3740163f3985d0f8046fe0810bb650d58
himanshutomar114 <himanshutomar114@gmail.com>
2025-07-03 17:04:40 +0530

    commit..

--- backend/src/server.js
+++ backend/src/server.js
@@ -42,13 +42,13 @@ app.use("/api/chat", chatRoutes);
 
 
 //this is for deployment of website
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
-
-    app.get("*", (req,res) =>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    })
-}
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 
 app.listen(PORT , () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,10 +3,10 @@
 import axios from "axios";
 
 //for deployment of website we use the base_url beacuse it changes the backend url on server
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5600/api" : "/api";
+
 
 export const axiosInstance = axios.create({
-    baseURL: BASE_URL, //backend server url
+    baseURL: "http://localhost:5600/api", //backend server url
     withCredentials: true //send cookies with request
 
 })
